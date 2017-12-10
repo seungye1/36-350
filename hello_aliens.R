375b5b405d11cd588dfe74a61541b1b6d94a5940
Sophia Yoo <sophiayoo@Sophias-MacBook-Pro.local>
2017-12-10 04:07:03 -0500

    Completed alien message.

---
+++ hello_aliens.R
@@ -0,0 +1,9 @@
+cat("Hello aliens, \n I'm a human from planet Earth. We, human beings,
+    live on land, and walk on two feet. We start as babies, and grow into
+    adults, and live in family structures, like many other animals do. We
+    aren't the only animals inhabiting Earth. We live in a civilized world,
+    usually inside buildings to protect ourselves from the crazy weather 
+    outside. How is your planet? Do you live outside? Or have you built
+    something to live inside of? What kinds of food do you eat to stay alive?
+    I was also wondering, what is your population? \n
+    Best, Sophia")
